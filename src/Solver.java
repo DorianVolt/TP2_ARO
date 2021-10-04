8626620a836b7e3af723a77ab497e82ba5e29699
Dorian <vdoralex@gmail.com>
2021-10-04 11:50:18 +0200

    FINAL commit 2

--- src/Solver.java
+++ src/Solver.java
@@ -25,6 +25,7 @@ public class Solver {
 
     public void Knapsack(int level, float weight, float profit, Bag bag, ArrayList<ObjectB> objects) {
         iterationCounter++;
+        //Condition d'arret
         if (level == objects.size()) {
             if (optimum < profit) {
                 optimum = profit;
@@ -32,13 +33,15 @@ public class Solver {
             }
             return;
         }
+        //On prend l'objet
+        //On verifie qu'il ne depasse pas la capacité
         if (objects.get(level).getWeight() + weight <= bag.getCapacity()) {
             currentIterationItems.add(objects.get(level));
             Knapsack(level + 1, weight + objects.get(level).getWeight(), profit + objects.get(level).getValue(), bag, objects);
             currentIterationItems.remove(currentIterationItems.size() - 1);
         }
-        List<ObjectB> nextItems = new ArrayList<>(objects);
-        nextItems = nextItems.subList(level + 1, objects.size());
+        //On verifie si explorer la branche vaut le coup
+        List<ObjectB> nextItems = new ArrayList<>(objects).subList(level + 1, objects.size());
         nextItems.addAll(currentIterationItems);
         if (glouton(nextItems, new Bag(bag.getCapacity())) >= optimum) {
             Knapsack(level + 1, weight, profit, bag, objects);
